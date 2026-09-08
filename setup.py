f3729a785a6fed57101c42f485519e3fcb0f497a
ev0xP <evoprimitive@gmail.com>
2026-09-08 15:58:47 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-import os
 from setuptools import setup, find_packages
 
 setup(
-    name='melotts',
-    version='0.1.2',
+    name="melotts",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -16,7 +15,7 @@ setup(
         "torch>=2.0.0",
         "torchaudio>=2.0.0",
         "mecab-python3>=1.0.8",
-        "unidic-lite>=1.0.8",  # Menggantikan unidic berat & menghilangkan 'python -m unidic download'
+        "unidic-lite>=1.0.8",
         "num2words>=0.5.12",
         "pypinyin>=0.49.0",
         "jieba>=0.42.1",
